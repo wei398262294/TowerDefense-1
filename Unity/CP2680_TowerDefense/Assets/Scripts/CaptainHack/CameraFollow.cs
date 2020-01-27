07851aafa41a0d605a3994bcf49dc6878dbd4cdb
Frederik <Frederikmallingthorsen@gmail.com>
2020-01-27 18:33:10 +0100

    Movement and cam
    Rotation yet to come

---
+++ Unity/CP2680_TowerDefense/Assets/Scripts/CaptainHack/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5f;
+    float minFov = 15f;
+    float maxFov = 90f;
+    float sensitivity = 20f;
+
+    Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        var fov = Camera.main.fieldOfView;
+        fov += -Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+    }
+
+}
--- Unity/CP2680_TowerDefense/Assets/Scripts/CaptainHack/PlayerMovement.cs
+++ Unity/CP2680_TowerDefense/Assets/Scripts/CaptainHack/PlayerMovement.cs
@@ -2,50 +2,129 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    
 
-    public float speed = 2.0f;
+    public float speed = 1000.0f;
+    public float rotationSpeed = 2.0f;
+    float camRayLength = 100f;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.freezeRotation = true;
+        rb.freezeRotation = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        Turning();
+
+        if (Input.GetKey(KeyCode.S))
         {
-            rb.velocity = transform.up * speed;
+            rb.velocity = transform.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.W))
         {
-            rb.velocity = -transform.up * speed;
+            rb.velocity = -transform.up * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.D))
         {   
-            rb.velocity = transform.right * speed;
-//            transform.Rotate(0, Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed, 0);
-//            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed);
-
+            rb.velocity = transform.right * speed * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.A))
         {
-            rb.velocity = -transform.right * speed;
+            rb.velocity = -transform.right * speed * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.velocity = transform.forward * speed;
+            rb.velocity = transform.forward * speed * Time.deltaTime;
         }
 
     }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast (camRay, out floorHit, camRayLength))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 90f;
+
+            Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+            rb.MoveRotation (newRotation);
+        }
+    }
+}
+
+*/
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    public float camRayLength = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask ("Floor");
+        playerRigidbody = GetComponent<Rigidbody> ();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw ("Horizontal");
+        float v = Input.GetAxisRaw ("Vertical");
+
+        Move (h, v);
+        Turning ();
+       // Animating (h, v);
+    }
+
+    void Move (float h, float v)
+    {
+        movement.Set (h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition (transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+            playerRigidbody.MoveRotation (newRotation);
+        }
+    }
+
+    /*
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool ("IsWalking", walking);
+    }
+    */
 }
